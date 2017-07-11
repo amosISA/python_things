a941f00e1c1900faddd6df85adb56a0b68d2f0fc
amosISA <amosisa700@gmail.com>
2017-07-11 10:35:20 +0200

    Prime factors algorithm changed

--- challenges/PrimeFactors_Challenge.py
+++ challenges/PrimeFactors_Challenge.py
@@ -1,51 +1,50 @@
-"""
-Write a function that finds the sum of all prime factors of a given number, n.
-Do it without using Trial Division (division por tentativa): An individual integer 
-being tested is called a trial divisor.
-This exercises is from codeplayer75939 in codeacademy. Not mine. 
-"""
-from math import sqrt
-#this function will sum the primes found:
-def sum(list):
-  values=0
-  for i in list:
-    values+=i
-  print values
-  return values
+from math import sqrt 
 
-#This function will find the primes in a number n:
-primes=[]
-def findprimes(n):
-  while n%2==0:                 #if our number n is even it will append a 2 factor and it will be divided until we get an odd number.
-    primes.append(2) 
-    n=n/2
-  i=3
-  while i<=sqrt(n):             #The tiniest prime factor of a number n must be equal or less than its square root. We add them while
-    while n%i==0:               #dividing it, so it will append every factor until we have the last factor (irreducible).
-      primes.append(i)
-      n=n/i
-    i+=2
-  if n>2:                          #Here we append the last factor.
-    primes.append(n)
-  
-findprimes(3)
-print "The list of primes is: "
-print primes
-print "And their sum is: "
-+sum(primes)
-
-#the next function filters the repeated primes:
-def sumdifferentprimes(list):
-  newlist=[]
-  for item in list:            #Basically it adds a number unless it was already added.
-    if item not in newlist:
-      newlist.append(item)
-  newsum=0
-  for i in newlist:
-    newsum+=i
-  print newlist
-  print newsum
-  return newsum
-print "The list with no repetitions and the sum:"
-sumdifferentprimes(primes)
-
+list = []
+def primeFactor(n):
+	if n > 1: 
+		# Divide by 2 /// 100 / 2 = 50 /// 50 / 2 = 25 === 2, 2
+		while n % 2 == 0: 
+			list.append(2)
+			n = n / 2
+			
+		# When exits first while n = 25 
+			
+		# When you cant divide by 2, you divide by next highest number, 3, and so on (i += 2), 5, 7 ... 
+		i = 3
+		while i <= sqrt(n): 
+			while n % i == 0: # 25 / 5 = 5 /// 5 / 5 = 1   === 2, 2, 5, 5 
+				list.append(i)
+				n = n / i 
+			i += 2
+				
+		# Here n = 1
+			
+		# If some number got his last prime factor such as 797, this will be added to the list as his last 
+		# (example prime factors of 9564)
+		if n > 2:
+			list.append(n);
+				
+		print list
+	else: 
+		print "Number should be higher than 1"
+		
+def primeFactorSum(list_n): 
+	values = 0 
+	for i in list_n: 
+		values += i 
+	return values 
+	
+def uniquePrimeFactors(list_n):
+	my_list = []
+	for i in list_n: 
+		if i not in my_list: 
+			my_list.append(i)
+	return my_list 
+	
+print "Prime factors:"
+primeFactor(100)
+print "Sum of prime factors are:"	
+print primeFactorSum(list)	
+print "Unique prime factors:"
+print uniquePrimeFactors(list)	
