c3ccce48f22ff2f854416c150e09bdb669477435
amosISA <amosisa700@gmail.com>
2017-07-11 12:57:22 +0200

    MaxProduc found in a list of integers

--- challenges/product_game.py
+++ challenges/product_game.py
@@ -10,3 +10,17 @@ def maxProductFinder(*args):
 	
 maxProductFinder(-8, 6, -7, 3, 2, 1, -9) 
 	
+	
+"""
+Given an array of integers, write a function, maxProductFinder, that finds the 
+largest product that can be obtained from ANY 3 integers in the array.
+"""
+def maxProd(x=[]):
+	x = sorted(x)
+	r1 = x[-1] * x[-2] * x[-3]
+	r2 = x[0] * x[1] * x[-1]
+	
+	return max(r1, r2)
+	
+print maxProd([-6, -8, 4, 2, 5, 3, -1, 9, 10])
+print maxProd([-8, 6, -7, 3, 2, 1, -9])
